1668d71a523e805a8213258ce019091844366925
paulac <cesarina.paula@dowjones.com>
2020-11-30 11:13:00 -0500

    initial commit

---
+++ bit_npm.sh
@@ -0,0 +1,4 @@
+echo "Adding bit.dev to npm registry"
+echo "@bit:registry=https://node.bit.dev" >> ~/.npmrc
+echo "//node.bit.dev/:_authToken=$BIT_TOKEN" >> ~/.npmrc
+echo "Completed adding bit.dev to npm registry"
--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ProductList from '@bit/cesarinapaula.testing.product-list'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>TESTING AND STUFF</p>
+        <ProductList/>
         <a
           className="App-link"
           href="https://reactjs.org"
